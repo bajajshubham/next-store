2dd0982334ff1efdf2fdf4c640135036a43f10b4
Shubham Bajaj <shubham2bajaj@gmail.com>
2026-02-28 20:38:58 +0100

    added sign up validator schema

--- lib/validators.ts
+++ lib/validators.ts
@@ -20,3 +20,14 @@ export const signInFormSchema = z.object({
   email: z.email({ error: 'Invalid email address' }),
   password: z.string().min(6, "Password must be at least 6 characters"),
 })
+
+export const signUpFormSchema = z.object({
+  name: z.string().min(2, "Name must me minimum 2 characters"),
+  email: z.email({ error: 'Invalid email address' }),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  confirmPassword: z.string().min(6, "Password must be at least 6 characters"),
+}).refine((data) => data.password === data.confirmPassword, {
+  error: "Passwords do not match",
+  path: ['confirmPassword']
+})
+
