b7dce26be34aa3f1b7d268dfd559a07ceeb8d3ee
Shubham Bajaj <shubham2bajaj@gmail.com>
2026-04-07 20:54:01 +0200

    displaying paypal payment buttons

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -15,7 +15,7 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
   if (!order) notFound()
 
   return (
-    <OrderDetailsTable order={{ ...order, shippingAddress: order.shippingAddress as ShippingAddress }} />
+    <OrderDetailsTable order={{ ...order, shippingAddress: order.shippingAddress as ShippingAddress }} paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'} />
   )
 }
 
