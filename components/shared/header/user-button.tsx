f5a6675305d1b5708ce20c7c779d8485f036d8e9
Shubham Bajaj <shubham2bajaj@gmail.com>
2026-04-08 11:02:12 +0200

    basic user layout

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -38,6 +38,16 @@ const UserButton = async () => {
             </div>
           </div>
         </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Link className="w-full" href="/user/profile">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link className='w-full' href='/user/orders'>
+            Order History
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuItem className="p-0 mb-1">
           <form action={signOutUser} className="w-full">
             <Button className="w-full py-4 px-2 h-4 justify-start" variant={"ghost"}>Sign out</Button>
