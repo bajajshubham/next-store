af2b521cf0c740f7de247d5402b934dfc9674d0b
Shubham Bajaj <shubham2bajaj@gmail.com>
2026-04-08 22:09:01 +0200

    user profile form

---
+++ app/user/profile/profile-form.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { toast } from 'sonner';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSession } from 'next-auth/react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { updateProfileSchema } from '@/lib/validators';
+import { updateProfile } from '@/lib/actions/user.actions';
+import { Field, FieldError } from '@/components/ui/field';
+
+const ProfileForm = () => {
+  const { data: session, update } = useSession();
+
+  const form = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+    defaultValues: {
+      name: session?.user?.name ?? '',
+      email: session?.user?.email ?? '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<z.infer<typeof updateProfileSchema>> = async (values: z.infer<typeof updateProfileSchema>) => {
+    console.log("HIIII")
+  }
+
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5">
+        <div className='flex flex-col gap-5'>
+          <Controller
+            control={form.control}
+            name='email'
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <Input
+                  {...field}
+                  disabled
+                  id="email"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Update your email"
+                  autoComplete="off"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          <Controller
+            control={form.control}
+            name='name'
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <Input
+                  {...field}
+                  id="name"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Firstname Lastname"
+                  autoComplete="off"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+        </div>
+        <Button
+          type='submit'
+          size='lg'
+          disabled={form.formState.isSubmitting}
+          className='button col-span-2 w-full'
+        >
+          {form.formState.isSubmitting ? 'Submitting...' : 'Update Profile'}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default ProfileForm;
